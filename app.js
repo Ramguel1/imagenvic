2e3d20512bc92c8ac34d8b70e7a474dc498dcfea
ramses <urielramsesgg@gmail.com>
2024-04-18 00:06:33 -0600

    op

---
+++ app.js
@@ -0,0 +1,59 @@
+var ima= document.querySelector("#ima");
+
+document.querySelector("#btn1").onclick=()=>{
+    ima.innerHTML="<img src='esc.jpg' width=300 height=300>";
+
+    ima.style.transform=" rotateY(-180deg)";
+ima.style.transition="all 0.5s linear";
+ima.style.transitionStyle="preserve-3d";
+ima.style.backfaceVisibility
+
+setTimeout(()=>{
+  ima.style.transform=" rotateY(0deg)";
+  ima.style.transition="all 0.5s linear";
+ima.src=ima
+},200)
+
+}
+document.querySelector("#btn2").onclick=()=>{
+    ima.innerHTML="<img src='acua.jpeg' width=300 height=300>";
+    ima.style.transform=" rotateY(-180deg)";
+ima.style.transition="all 0.5s linear";
+ima.style.transitionStyle="preserve-3d";
+ima.style.backfaceVisibility
+
+setTimeout(()=>{
+  ima.style.transform=" rotateY(0deg)";
+  ima.style.transition="all 0.5s linear";
+ima.src=ima
+},200)
+
+}
+document.querySelector("#btn3").onclick=()=>{
+    ima.innerHTML="<img src='can.webp' width=300 height=300>";
+    ima.style.transform=" rotateY(-180deg)";
+ima.style.transition="all 0.5s linear";
+ima.style.transitionStyle="preserve-3d";
+ima.style.backfaceVisibility
+
+setTimeout(()=>{
+  ima.style.transform=" rotateY(0deg)";
+  ima.style.transition="all 0.5s linear";
+ima.src=ima
+},200)
+
+}
+document.querySelector("#btn4").onclick=()=>{
+    ima.innerHTML="<img src='cap.jpeg' width=300 height=300>";
+    ima.style.transform=" rotateY(-180deg)";
+ima.style.transition="all 0.5s linear";
+ima.style.transitionStyle="preserve-3d";
+ima.style.backfaceVisibility
+
+setTimeout(()=>{
+  ima.style.transform=" rotateY(0deg)";
+  ima.style.transition="all 0.5s linear";
+ima.src=ima
+},200)
+
+}
